ecb166b0f7fdfd4a92bde4b28412a743d7ef3f25
Arpon das <arpon.front@gmail.com>
2025-11-06 00:36:18 +0600

    second day of my static-portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,11 @@
 window.addEventListener("scroll", () => {
-  const scrollDoc = document.documentElement;
-  const maxScroll = scrollDoc.scrollHeight - scrollDoc.clientHeight;
-  const scrollTop = Math.round(scrollDoc.scrollTop);
-  const imgId = document.getElementById('intro');
-  const imgIdWidth = parseFloat(window.getComputedStyle(imgId).width);
-  const playAble = scrollDoc.clientWidth - imgIdWidth;
-//   console.log(`scrollHeight ${scrollTop.scrollHeight}`); //element height
-//   console.log(` clientWidth ${scrollDoc.clientWidth}`); //viewport width without scrollbar
-//   console.log(`innerWidth ${window.innerWidth}`); //viewport width with scrollbar
-//   console.log(`scrollTop ${Math.round(scrollTop.scrollTop)}`); //scroll from top
-let progress = 0;
-if (maxScroll > 0) {
-    progress = (scrollTop / maxScroll) * 100;
-}
-progress = Math.min(100, Math.max(0, progress));
+  const htmlEle = document.documentElement;
+  const scrollTop = Math.round(htmlEle.scrollTop) ;
+  const viewportHeight = htmlEle.clientHeight;
+  const htmlEleHeight = htmlEle.scrollHeight;
+  const possibleScroll = htmlEleHeight - viewportHeight;
+  let progress = 0;
 
-// console.log(progress);
-let transform = 0;
-
-if (playAble) {
-    transform = playAble * (progress / 100);
-// console.log(transform);
-}
-transform = Math.min(playAble, Math.max(0, transform));
-
-console.log(transform);
-
-imgId.style.translate = transform + 'px';
+  progress = (scrollTop / possibleScroll) * 100;
+  console.log(progress);
 });
