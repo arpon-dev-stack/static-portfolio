205e1f51329b76b94a3f0a3618067ba76138c8b8
Arpon das <arpon.front@gmail.com>
2025-11-07 12:35:08 +0600

    The sixth commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,28 @@
+const imgId = document.getElementById("portImg");
+const imgContainer = document.getElementById("intro");
+const img_name = document.getElementById("intro_name");
+
+// portfolio image hover effect
+
+
+imgContainer.addEventListener("mouseenter", () => {
+  img_name.style.display = 'block';
+});
+imgContainer.addEventListener("mouseleave", () => {
+  console.log("hover over");
+  img_name.style.display = 'none';
+});
+
 window.addEventListener("scroll", () => {
   const htmlEle = document.documentElement;
-  const scrollTop = Math.round(htmlEle.scrollTop) ;
+  const scrollTop = Math.round(htmlEle.scrollTop);
   const viewportHeight = htmlEle.clientHeight;
   const htmlEleHeight = htmlEle.scrollHeight;
   const possibleScroll = htmlEleHeight - viewportHeight;
-  const imgId = document.getElementById('portImg');
   let progress = 0;
 
   progress = (scrollTop / possibleScroll) * 100;
-  imgId.style.transform = `rotate(${360 * (progress / 100) * 7}deg)`;
+
+  imgId.style.transform = `rotate(${((360 * (progress / 100)) * 7)}deg)`;
 });
+
